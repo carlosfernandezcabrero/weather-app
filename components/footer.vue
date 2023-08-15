2cf00c840d77a1bb77537bc5c33a0e07b18461ba
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-08-15 14:49:01 +0200

    add links

--- components/footer.vue
+++ components/footer.vue
@@ -1,5 +1,25 @@
 <template>
-  <footer class="py-12 text-center bg-primary mt-32 w-full">
-    <p>Iconos de <a href="https://icons8.com/" target="_blank">Icons8</a></p>
+  <footer
+    class="py-12 text-center bg-primary mt-32 w-full flex flex-col gap-y-2"
+  >
+    <p>
+      Iconos de
+      <a class="font-bold" href="https://icons8.com/" target="_blank">Icons8</a>
+    </p>
+    <p>
+      Ideas cogidas de
+      <a class="font-bold" href="https://github.com/ikurotime" target="_blank">
+        David Huertas
+      </a>
+    </p>
+    <p>
+      <a
+        class="text-xl font-bold"
+        href="https://github.com/carlosfernandezcabrero"
+        target="_blank"
+      >
+        GitHub
+      </a>
+    </p>
   </footer>
 </template>
