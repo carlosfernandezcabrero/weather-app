643a5e24947e4c71f09691e6a2397824b91c44a8
Carlos Fernandez Cabrero <carlitostrabajo2018@outlook.es>
2023-12-10 13:05:20 +0100

    Add title to app head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,6 +27,7 @@ export default defineNuxtConfig({
 
   app: {
     head: {
+      title: 'Weather App',
       script: [
         {
           src: `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&libraries=places`
